4fdb07145cd2d9eb922e78e17985d1259d676d3d
Nadia <10745485+n-martynova@users.noreply.github.com>
2020-06-17 18:27:45 +0300

    deploy test

--- src/main.js
+++ src/main.js
@@ -1,39 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
-// import Stampede from 'stampede2/src/stampede2'
-// import VueHead from 'vue-head'
 
 import 'fullpage.js/vendors/scrolloverflow'
 import 'fullpage.js/dist/fullpage.css'
 import VueFullPage from 'vue-fullpage.js'
 
-// import router from './router'
-
-// Vue.use(VueHead)
 Vue.use(VueFullPage)
-// Vue.use(Stampede, {
-//     view: 'with-button',
-
-//     bg_color: 'transparent',
-//     btn_text: 'Ок',
-//     btn_text_color: '#000',
-//     accent_color: '#FFF',
-//     text_color: '#000',
-//     spinner_size: 60,
-
-//     api_url: 'https://rocketbank.ru/api/marketing/orders/2019', // test
-
-//     phone_step: {
-//         title: 'Телефон',
-//         text: 'Введите номер телефона',
-//         gtag: 'send phone'
-//     }
-// })
 
 Vue.config.productionTip = false
 
 new Vue({
-    // router,
     render: h => h(App),
-  mounted: () => document.dispatchEvent(new Event("x-app-rendered")),
+    mounted: () => document.dispatchEvent(new Event("x-app-rendered")),
 }).$mount('#app')
